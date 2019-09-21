f5401a3ecf5d2b0838ffd32be7073b2827449f3d
astraizer <kevinkrisnadp98@gmail.com>
2019-09-21 10:07:34 +0700

    Initial Commit

---
+++ Arctic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Arctic
+//
+//  Created by Nostra on 21/09/19.
+//  Copyright © 2019 Software Laboratory Center. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
